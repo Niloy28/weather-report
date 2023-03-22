b141a2a354e86bec3e0126675f051916a47aa9e1
Farhan Ishrak Islam <niloy28@live.com>
2023-03-22 09:53:06 +0600

    Added useFetch hook.

---
+++ src/hooks/use-fetch.ts
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+
+const useFetch = <T>(requestInfo: Request, transform?: (data: any) => T) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [fetchError, setFetchError] = useState<string | null>(null);
+	const [data, setData] = useState<T | null>(null);
+
+	const fetchData = useCallback(async () => {
+		setIsLoading(true);
+		const response = await fetch(requestInfo);
+		if (response.ok) {
+			const data = await response.json();
+			if (requestInfo.method === "GET" && transform) {
+				setData(transform(data));
+			}
+			setIsLoading(false);
+		} else {
+			setIsLoading(false);
+			setFetchError(response.statusText);
+		}
+	}, [requestInfo, transform]);
+
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+
+	return {
+		isLoading,
+		data,
+		fetchError,
+	};
+};
+
+export default useFetch;
