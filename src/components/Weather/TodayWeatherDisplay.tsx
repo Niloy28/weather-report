e4b03c37beec8a80cbe9eb6470b855da11fd2418
Farhan Ishrak Islam <niloy28@live.com>
2023-03-25 23:52:45 +0600

    Migrate to Vite app.

--- src/components/Weather/TodayWeatherDisplay.tsx
+++ src/components/Weather/TodayWeatherDisplay.tsx
@@ -10,56 +10,58 @@ import AuxiliaryWeatherDataDisplay from "./AuxiliaryWeatherDataDisplay";
 import Card from "../UI/Card";
 
 const TodayWeatherDisplay = () => {
-	// TODO: use geocoding API to get location
-	// 			 and then fetch weather data
-	const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=23.7644025&lon=90.389015&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`;
+  // TODO: use geocoding API to get location
+  // 			 and then fetch weather data
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=23.7644025&lon=90.389015&units=metric&appid=${
+    import.meta.env.VITE_OPENWEATHER_API_KEY
+  }`;
 
-	const weatherDataTransformer = useCallback((data: any) => {
-		let weatherData: CurrentWeatherData;
+  const weatherDataTransformer = useCallback((data: any) => {
+    let weatherData: CurrentWeatherData;
 
-		weatherData = {
-			weatherStateData: {
-				main: data.weather[0].main,
-				description: data.weather[0].description,
-				icon: data.weather[0].icon,
-				tempMin: Math.round(data.main.temp_min),
-				tempMax: Math.round(data.main.temp_max),
-			},
-			temperatureData: {
-				realTemperature: Math.round(data.main.temp),
-				feelTemperature: Math.round(data.main.feels_like),
-				epochTime: data.dt,
-			},
-			auxiliaryWeatherData: {
-				pressure: data.main.pressure,
-				humidity: data.main.humidity,
-				windSpeed: data.wind.speed,
-				windAngle: data.wind.deg,
-			},
-		};
+    weatherData = {
+      weatherStateData: {
+        main: data.weather[0].main,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon,
+        tempMin: Math.round(data.main.temp_min),
+        tempMax: Math.round(data.main.temp_max),
+      },
+      temperatureData: {
+        realTemperature: Math.round(data.main.temp),
+        feelTemperature: Math.round(data.main.feels_like),
+        epochTime: data.dt,
+      },
+      auxiliaryWeatherData: {
+        pressure: data.main.pressure,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        windAngle: data.wind.deg,
+      },
+    };
 
-		return weatherData;
-	}, []);
+    return weatherData;
+  }, []);
 
-	const weatherRequest = useMemo(() => new Request(weatherUrl), [weatherUrl]);
-	const { isLoading, data, fetchError } = useFetch<CurrentWeatherData>(
-		weatherRequest,
-		weatherDataTransformer
-	);
+  const weatherRequest = useMemo(() => new Request(weatherUrl), [weatherUrl]);
+  const { isLoading, data, fetchError } = useFetch<CurrentWeatherData>(
+    weatherRequest,
+    weatherDataTransformer
+  );
 
-	if (isLoading || !data) {
-		return <p>Loading...</p>;
-	}
+  if (isLoading || !data) {
+    return <p>Loading...</p>;
+  }
 
-	return (
-		<div className={styles["today-weather"]}>
-			<Card className={styles["weather-main"]}>
-				<WeatherStateDisplay {...data.weatherStateData} />
-				<TemperatureDisplay {...data.temperatureData} />
-			</Card>
-			<AuxiliaryWeatherDataDisplay {...data.auxiliaryWeatherData} />
-		</div>
-	);
+  return (
+    <div className={styles["today-weather"]}>
+      <Card className={styles["weather-main"]}>
+        <WeatherStateDisplay {...data.weatherStateData} />
+        <TemperatureDisplay {...data.temperatureData} />
+      </Card>
+      <AuxiliaryWeatherDataDisplay {...data.auxiliaryWeatherData} />
+    </div>
+  );
 };
 
 export default TodayWeatherDisplay;
