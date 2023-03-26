9a37fd9f018b55254b1a67f601913875d9bd5b42
Farhan Ishrak Islam <niloy28@live.com>
2023-03-27 00:54:39 +0600

    Fix useFetch hook.

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,34 +1,36 @@
 import { useCallback, useEffect, useState } from "react";
 
 const useFetch = <T>(requestInfo: Request, transform?: (data: any) => T) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [fetchError, setFetchError] = useState<string | null>(null);
-	const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [data, setData] = useState<T | null>(null);
 
-	const fetchData = useCallback(async () => {
-		setIsLoading(true);
-		const response = await fetch(requestInfo);
-		if (response.ok) {
-			const data = await response.json();
-			if (requestInfo.method === "GET" && transform) {
-				setData(transform(data));
-			}
-			setIsLoading(false);
-		} else {
-			setIsLoading(false);
-			setFetchError(response.statusText);
-		}
-	}, [requestInfo, transform]);
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    const response = await fetch(requestInfo);
+    if (response.ok) {
+      const data = await response.json();
+      if (requestInfo.method === "GET" && transform) {
+        setData(transform(data));
+      } else {
+        setData(data);
+      }
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+      setFetchError(response.statusText);
+    }
+  }, [requestInfo, transform]);
 
-	useEffect(() => {
-		fetchData();
-	}, [fetchData]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-	return {
-		isLoading,
-		data,
-		fetchError,
-	};
+  return {
+    isLoading,
+    data,
+    fetchError,
+  };
 };
 
 export default useFetch;
